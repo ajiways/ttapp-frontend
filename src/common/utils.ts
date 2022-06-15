b6cafe8b39795838553d9aeb5e1d3db250df29ef
ajiways <ajiwayswc@gmail.com>
2022-06-15 20:01:53 +0600

    feature/ useSnackBar hook added

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-useless-escape */
 
+import { EResponseType } from "./enums";
+
 export function notEmpty<TValue>(
   value: TValue | null | undefined
 ): value is TValue {
@@ -25,4 +27,38 @@ export function getCurrentWeekType() {
     dd = d.getDay(),
     re = Math.floor((d0 - d1) / 8.64e7) + (dd ? dd - 1 : 6);
   return Math.floor(re / 7) % 2 ? true : false;
+}
+
+export function useSnackbar(
+  res:
+    | {
+        type: EResponseType;
+        message: string;
+        [key: string]: unknown;
+      }
+    | undefined,
+  key?: string
+): [type: EResponseType, message: string, show: boolean, arg?: unknown] {
+  let type = EResponseType.ERROR;
+  let show = false;
+  let message = "";
+
+  if (!res || (key && !res[key])) {
+    type = EResponseType.ERROR;
+    message = "Произошла непредвиенная ошибка, попробуйте позже";
+    show = true;
+    return [type, message, show];
+  }
+
+  if (res.type === EResponseType.ERROR) {
+    type = EResponseType.ERROR;
+    message = res.message;
+    show = true;
+    return [type, message, show];
+  }
+
+  type = EResponseType.SUCCESS;
+  message = res.message;
+  show = true;
+  return [type, message, show, res.updatedLessons];
 }
